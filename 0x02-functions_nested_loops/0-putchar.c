d770277e39ebd16a33c65f7f17e8784a83759263
AdomBoateng <craxybabooon6@gmail.com>
2023-07-18 14:48:14 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
-#!/bin/bash
-#include <stdio.h>
-/*
- * Code to print _putchar followed by a new line
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
-int main() {
-    printf("_putchar\n");
-    return 0;
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
